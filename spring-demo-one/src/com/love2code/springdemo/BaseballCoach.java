a3ab7fdc3d68313f85bfda5122e8a048e0f823d7
ErasylAbenov <abenov.erasyl@gmail.com>
2020-05-24 20:38:36 +0600

    Code Demo - Rough Prototype Part 1

---
+++ spring-demo-one/src/com/love2code/springdemo/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.love2code.springdemo;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Spend 30 minutes on batting practice";
+	}
+}
