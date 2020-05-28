eed99a2aaec33e8ff15e672cff4063b263c0537b
ErasylAbenov <abenov.erasyl@gmail.com>
2020-05-28 22:59:35 +0600

    Setter Injection - Write Some Code - Part 2

---
+++ spring-demo-one/src/com/love2code/springdemo/SetterDemoApp.java
@@ -0,0 +1,25 @@
+package com.love2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from spring container
+		CricketCoach theCoach = context.getBean("myCrickedCoach", CricketCoach.class);
+		
+		// call methods on the bean
+		// ... let's come back to this ...
+		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyFortune());
+		
+		// close the context
+		context.close();
+	}
+
+}
