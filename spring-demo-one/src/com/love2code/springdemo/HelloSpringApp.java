8266daaf439be812239bc923c8af54ddb9d632dd
ErasylAbenov <abenov.erasyl@gmail.com>
2020-05-28 22:27:35 +0600

    Spring Dependency Injection - Write Some Code - Part 3

--- spring-demo-one/src/com/love2code/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/com/love2code/springdemo/HelloSpringApp.java
@@ -5,12 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// load the spring configuration file
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrieve a bean from spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		
+		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		
+		// lets call our new methods for fortune
+		System.out.println(theCoach.getDailyFortune());
 		
 		context.close(); 
 	}
--- spring-demo-one/src/com/love2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/love2code/springdemo/TrackCoach.java
@@ -1,6 +1,16 @@
 package com.love2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+		
+	}
+	
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -11,7 +21,7 @@ public class TrackCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Just Do It " + fortuneService.getFortune();
 	}
 
 }
