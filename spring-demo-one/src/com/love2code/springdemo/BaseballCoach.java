401ffc2b85ca295bd70776c8b69b6986f8199103
ErasylAbenov <abenov.erasyl@gmail.com>
2020-05-28 22:09:29 +0600

    Spring Dependency Injection - Write Some Code - Part 2

--- spring-demo-one/src/com/love2code/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/love2code/springdemo/BaseballCoach.java
@@ -1,16 +1,26 @@
 package com.love2code.springdemo;
 
 public class BaseballCoach implements Coach {
+	
+	// define a private field for the dependency
+	private FortuneService fortuneService;
 
+	
+	// define a constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
 		return "Spend 30 minutes on batting practice";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// use my fortuneService to get a fortune
+		
+		return fortuneService.getFortune();
 	}
 }
